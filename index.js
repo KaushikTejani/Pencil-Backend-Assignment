f273d152c49f2e0ca887ce9f29edfddcb09f8c0a
Kaushik Tejani <tejanikaushik1999@gmail.com>
2022-07-28 08:03:04 +0530

    DB Connected

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://Kaushik:Kaushik@cluster0.rpenozr.mongodb.net/Pencil?retryWrites=true&w=majority', (err) => {
+    if (err)
+        console.log(err);
+    else
+        console.log("DB Connected");
+});
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.send('Hello World')
 })
 
-app.listen(PORT,() => console.log(`Server start on port no ${PORT}`));
+app.listen(PORT, () => console.log(`Server start on port no ${PORT}`));
